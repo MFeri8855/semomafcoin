5e3776403b641ae268c4aa1621ea42dc3fda2a73
semomaf <113037803+MFeri8855@users.noreply.github.com>
2023-07-22 14:07:02 +0330

    Update index.js
    change contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x99b0ef3025f2c34889eb35fe90a1ede02a8aa0f2";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
